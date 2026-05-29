c7a3fa478a7e47d23fd1f03cca1d35bbfabad4ba
chebitoch007 <eliphazchebitoch@gmail.com>
2026-05-29 21:15:50 +0300

    Optimize Next.js and Vercel configuration

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [],
+    unoptimized: true,
   },
+  productionBrowserSourceMaps: false,
+  swcMinify: true,
+  poweredByHeader: false,
+  compress: true,
+  generateEtags: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 };
 
 module.exports = nextConfig;
